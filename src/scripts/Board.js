314b3b52f46f75f6cee911ddb48f2fdb0c439f5b
Jax <jiaxingrong66@gmail.com>
2026-02-05 10:32:55 -0500

    Initialization of project structure

---
+++ src/scripts/Board.js
@@ -0,0 +1,23 @@
+class Board {
+  constructor(boardSize, cellSize) {
+    this.boardSize = boardSize;
+    this.cellSize = cellSize;
+  }
+
+  draw() {
+    fill(0, 0, 255);
+
+    for (let x = 0; x < this.boardSize; x++) {
+      for (let y = 0; y < this.boardSize; y++) {
+        rect(
+          x * this.cellSize,
+          y * this.cellSize,
+          this.cellSize,
+          this.cellSize,
+        );
+      }
+    }
+  }
+}
+
+export { Board };
